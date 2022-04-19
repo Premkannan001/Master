f0fa50d2fa66dc841a7a81348028771bd2997d51
premkannan001 <premkannan001@gmail.com>
2022-04-19 12:43:24 +0530

    project structure

---
+++ src/org/datatype/DataType.java
@@ -0,0 +1,42 @@
+package org.datatype;
+
+public class DataType {
+	public static void main(String[] args) {
+		byte age=127;
+		System.out.println(age);
+		System.out.println("my age is "+age);
+		short atmpin=32767;
+		System.out.println(atmpin);
+		System.out.println("my atmpin is "+atmpin);
+		int pincode =0000001;
+		System.out.println(pincode);
+		System.out.println("my pincode is "+pincode);
+		long phonenumber=962685648878787889l;
+	    System.out.println(phonenumber);
+	    System.out.println("my phone number is "+phonenumber);
+	    float salary=8778646115986784378f;
+	    System.out.println(salary);
+	    System.out.println("my salary is "+salary);
+	    double a=877864611;
+	    System.out.println(a);
+	    System.out.println("my a is "+a);
+	    char gender='8';
+	    System.out.println(gender);
+	    System.out.println("my gender is "+gender);
+	    String email ="premkumar@gmail.com";
+	    System.out.println(email);
+	    System.out.println("premkumar@gmail.com is my "+email);
+	    boolean result=true;
+	    System.out.println(result);
+	   System.out.println("my result is "+true);
+	    
+	
+	
+	
+
+	
+	}
+
+	}
+
+
